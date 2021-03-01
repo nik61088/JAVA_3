92477da8f211aa89dc2c0e26c5ca19379569dc46
nik61088 <nik61088@gmail.com>
2021-03-01 18:36:09 +0300

    Lesson4

---
+++ src/main/java/Lesson4/Lesson4.java
@@ -0,0 +1,78 @@
+package Lesson4;
+
+public class Lesson4 {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+    private int x = 5;
+
+    public static void main(String[] args) {
+        Lesson4 w = new Lesson4();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        Thread t3 = new Thread(() -> {
+            w.printC();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < x; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < x; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < x; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    System.out.println();
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+
